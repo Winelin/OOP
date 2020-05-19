ff074cffceceb0a6826c61e0bb9817b1e161e20c
Alexandra Yankina <51386590+Winelin@users.noreply.github.com>
2020-05-19 20:24:32 +0300

    Add files via upload

---
+++ MoscowTransport.WebService/DomainObjects/DOGobj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOGobj.DomainObjects
+{
+    public class DOGobj : DomainObject
+    {
+        public string DepartmentalAffiliation { get; set; }
+        public string SiteElements { get; set; }
+        public string PlatformLighting { get; set; }
+
+        public string Landmark { get; set; }
+
+        public string FencingSite { get; set; }
+
+
+
+    }
+}
