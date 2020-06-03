6d40eada3ce8fbc858767662bc4cb5f34b255839
Alexandra Yankina <51386590+Winelin@users.noreply.github.com>
2020-06-03 17:03:36 +0300

    Add files via upload

---
+++ DOGobj.GeneralLogic/ApplicationServices/GetDOGobjListUseCase/GetDOGobjListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using DOGobj.DomainObjects;
+using DOGobj.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOGobj.ApplicationServices.GetPlatformLightingListUseCase
+{
+    public class GetDOGobjListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<dogobj> DOGobjs { get; }
+
+        public GetDOGobjListUseCaseResponse(IEnumerable<dogobj> dogobjs) => DOGobjs = dogobjs;
+    }
+}
---
+++ DOGobj.GeneralLogic/ApplicationServices/Ports/Gateways/Database/IDOGobjDatabaseGateway.cs
@@ -0,0 +1,25 @@
+﻿using DOGobj.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOGobj.ApplicationServices.Ports.Gateways.Database
+{
+    public interface IDOGobjDatabaseGateway
+    {
+        Task AddDOGobj(dogobj dogobj);
+
+        Task RemoveDOGobj(dogobj dogobj);
+
+        Task UpdateDOGobj(dogobj dogobj);
+
+        Task<dogobj> GetDOGobj(long id);
+
+        Task<IEnumerable<dogobj>> GetAllDOGobjs();
+
+        Task<IEnumerable<dogobj>> QueryDOGobjs(Expression<Func<dogobj, bool>> filter);
+
+    }
+}
